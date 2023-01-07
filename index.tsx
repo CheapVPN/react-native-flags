6b1ca413233eab9ced5ddb2e64b3c0ec6097abbd
Valentyn Berehovyi <valentyn.berehovyi@gmail.com>
2023-01-07 22:37:46 +0200

    Added testID

--- index.tsx
+++ index.tsx
@@ -5,11 +5,12 @@ import * as flags from './flags';
 type Props = {
   size: 16 | 24 | 32 | 48 | 64,
   code: string,
+  testID?: string;
   type?: 'flat' | 'shiny',
   style?: ImageStyle,
 };
 
-const Flag: React.FC<Props> = ({ size = 64, code, type = 'shiny', style }) => {
+const Flag: React.FC<Props> = ({ size = 64, code, testID, type = 'shiny', style }) => {
   const flag = flags[type][`icons${size}`][code];
   const unknownFlag = flags[type][`icons${size}`]['unknown'];
 
@@ -17,6 +18,7 @@ const Flag: React.FC<Props> = ({ size = 64, code, type = 'shiny', style }) => {
     <Image
       source={flag || unknownFlag}
       style={[{ width: size, height: size }, style]}
+      testID={testID}
     />
   );
 };
